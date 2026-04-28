877d354236f4636677177e95ce27e0200bc59caa
Nochi <255517211+Nochi12@users.noreply.github.com>
2026-04-28 18:20:58 +0800

    finish the step 1
    -added a array to store books
    -add a unique book id using crypto.randomUUID
    -create a book constructor &  function to create books to put in on array

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-let test = "Hello World";
+const myLibrary = [];
 
-console.log(test)
+function Book( title, author, pages, id ) {
+  // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.id  =  self.crypto.randomUUID();
+  
+}
+
+function addBookToLibrary( title, author, pages ) {
+
+    const create_Book = new Book( title, author, pages );
+    const push_Book = myLibrary.push(create_Book);
+    return push_Book
+}
+
+addBookToLibrary("Atomic Habits","James Clear", 200)
+addBookToLibrary("Lord of the rings","tolkien", 500 )
+console.log(myLibrary)
